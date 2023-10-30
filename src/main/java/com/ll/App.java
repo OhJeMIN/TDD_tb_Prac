0a7dfd721be7a9396ee8e8c6aaa68d923dd1091d
ohJeMin <61536578+OhJeMIN@users.noreply.github.com>
2023-10-30 22:05:19 +0900

    목록 입력하면 목록이 번호 /작가 /명언 순으로 나오게 한다. (#6)

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,16 +1,19 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     private Scanner scanner;
     private int lastQuotationId = 0;
+    private ArrayList<Quotation> quotations;
     App(Scanner scanner) {
         this.scanner = scanner;
     }
 
     void run() {
         System.out.println("== 명언 앱 ==");
+        quotations = new ArrayList<>();
         while (true){
             System.out.println("명령) :");
             String cmd = scanner.nextLine();
@@ -22,8 +25,18 @@ public class App {
                 String content = scanner.nextLine();
                 System.out.println("작가) : ");
                 String authorName = scanner.nextLine();
-                lastQuotationId++;
-                System.out.println(lastQuotationId+"번 명언이 등록되었습니다.");
+                int id = lastQuotationId++;
+                Quotation quotation = new Quotation(id,content,authorName);
+                quotations.add(quotation);
+                System.out.println(quotation.getId()+"번 명언이 등록되었습니다.");
+            }
+            else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i=quotations.size()-1;i>=0;i--){
+                    Quotation quotation = quotations.get(i);
+                    System.out.println(quotation.getId()+1 + " / " + quotation.getAuthorName()+ " / " + quotation.getContent());
+                }
             }
         }
     }
