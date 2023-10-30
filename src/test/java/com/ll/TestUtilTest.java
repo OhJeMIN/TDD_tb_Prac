4f80f138b29f7fc1ad68c05a4789cfa95f0c39f3
jasonoh22 <jasonoh22@naver.com>
2023-10-30 21:18:18 +0900

    TestUtil.setOutToByteArray()

--- src/test/java/com/ll/TestUtilTest.java
+++ src/test/java/com/ll/TestUtilTest.java
@@ -3,6 +3,7 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +26,20 @@ public class TestUtilTest {
         assertThat(authorName).isEqualTo("박명수");
     }
 
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray() 테스트")
+    void t2(){
+        //모니터에 대한 출력을 끄는 것
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 박명수 / 어려운 길은 길이 아니다.");
+        String out = byteArrayOutputStream.toString().trim();
+
+        assertThat(out).isEqualTo("2 / 박명수 / 어려운 길은 길이 아니다.");
+        //모니터에 대한 출력을 다시 키는 것
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        System.out.println("이제 다시 정상 출력");
+    }
 
 }
